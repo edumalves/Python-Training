c544ddc4c66a31267a4a12ed506e99e6a99a9c0f
Eduardo Alves <ealves@me.com>
2021-07-28 06:20:20 -0300

    Working PC Versions

--- PythonLearnings.py
+++ PythonLearnings.py
@@ -3,5 +3,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 stockdata = pd.read_csv('neogrid.csv', infer_datetime_format = False, parse_dates = ['Date'], index_col='Date')
-stockdata.to_excel('NeoGrid.xlsx')
-stockdata
+plt.plot(stockdata['Close'])
